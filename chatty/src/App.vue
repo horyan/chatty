f67056f4fa54bd56a88b521aa7365a3ecfb20613
Ryan Ho <ryan.ho@ucla.edu>
2020-02-26 08:45:53 -0500

    Created Vue project, added basic chat screen and functionality

---
+++ chatty/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <MessagesList :messageList="messages" />
+    <input type="text" placeholder="Enter a message" v-model="newMessage">
+    <button @click="sendMessage(newMessage)">Send Message</button>
+  </div>
+</template>
+
+<script>
+import MessagesList from "./components/MessagesList";
+
+export default {
+  name: 'App',
+  components: {
+    MessagesList
+  },
+  data() {
+    return {
+      messages: [],
+      newMessage: ''
+    }
+  },
+  methods: {
+    sendMessage(newMessage) {
+      this.messages = [...this.messages, newMessage];
+      this.newMessage = '';
+    }
+  }
+}
+</script>
---
+++ chatty/src/components/MessagesList.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul>
+    <li v-for="(message, index) in messageList" :key="index">{{message}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['messageList']
+};
+</script>
