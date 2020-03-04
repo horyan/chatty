35519b8a4ce4f57b0ff6e1bf880be8cccfc0d0d6
Ryan Ho <ryan.ho@ucla.edu>
2020-03-04 10:39:26 -0500

    added containers and styling to input and button

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <MessagesList :messageList="messages" />
-    <input type="text" placeholder="Enter a message" v-model="newMessage" />
-    <button @click="sendMessage(newMessage)">Send Message</button>
+  <div class="flex mx-auto h-screen items-center w-1/2">
+    <div class="w-full">
+      <MessagesList :messageList="messages"/>
+      <div class="flex">
+        <input class="flex-1 bg-white border border-blue-500 focus:outline-none focus:border-blue-700 py-2 px-4 appearance-none" type="text" placeholder="Enter a message" v-model="newMessage" />
+        <button class="bg-blue-500 border border-blue-500 hover:border-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4" @click="sendMessage(newMessage)">Send Message</button>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { firestorePlugin } from 'vuefire'
-
-Vue.config.productionTip = false
+import { firestorePlugin } from "vuefire";
+import './assets/styles.css';
 
 Vue.use(firestorePlugin)
+
+Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
