9424d36ecc505ded82ef87b9616a39ec81e956d0
Ryan Ho <ryan.ho@ucla.edu>
2020-03-04 21:21:40 -0500

    trialing chat border, added logic to validate message input, and changed method parameter for consistency

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="flex mx-auto h-screen items-center w-1/2">
-    <div class="w-full">
+    <div class="border px-4 py-4 w-full">
       <MessagesList :messageList="messages"/>
       <div class="flex">
-        <input class="flex-1 bg-white border border-blue-500 focus:outline-none focus:border-blue-700 py-2 px-4 appearance-none" type="text" placeholder="Enter a message" v-model="newMessage" />
+        <span v-if="!isValidMessage">Please enter valid message</span>
+        <input class="flex-1 bg-white border border-blue-500 focus:outline-none focus:border-blue-700 py-2 px-4 appearance-none" type="text" placeholder="Enter a message" v-model="newMessage"/>
         <button class="bg-blue-500 border border-blue-500 hover:border-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4" @click="sendMessage(newMessage)">Send Message</button>
       </div>
     </div>
@@ -22,17 +23,25 @@ export default {
   data() {
     return {
       messages: [],
-      newMessage: ""
+      newMessage: "",
+      isValidMessage: true
     };
   },
   firestore: {
     messages: db.collection("messages").orderBy("createdAt")
   },
   methods: {
-    sendMessage(newMessage) {
+    sendMessage(message) {
+      if (message === "") {
+        this.isValidMessage = false;
+        return
+      }
+      else {
+        this.isValidMessage = true;
+      }
       db.collection("messages").add({
         createdAt: new Date(),
-        value: newMessage
+        value: message
       });
       this.newMessage = "";
     }
