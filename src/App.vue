818efbdd53cd13455cb17788802d1497cca04183
Ryan Ho <ryan.ho@ucla.edu>
2020-03-04 22:18:53 -0500

    store random names generated by faker.js library in firebase, display name for users, and prepend chat names to displayed messages

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div class="flex mx-auto h-screen items-center w-1/2">
     <div class="border px-4 py-4 w-full">
       <MessagesList :messageList="messages"/>
-      <div class="flex">
+      Your automatically generated chat name: {{name}}
+      <div class="flex flex-wrap">
         <span v-if="!isValidMessage">Please enter valid message</span>
         <input class="flex-1 bg-white border border-blue-500 focus:outline-none focus:border-blue-700 py-2 px-4 appearance-none" type="text" placeholder="Enter a message" v-model="newMessage"/>
         <button class="bg-blue-500 border border-blue-500 hover:border-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4" @click="sendMessage(newMessage)">Send Message</button>
@@ -12,6 +13,7 @@
 </template>
 
 <script>
+import { name } from 'faker';
 import MessagesList from "./components/MessagesList";
 import { db } from "./db";
 
@@ -24,11 +26,15 @@ export default {
     return {
       messages: [],
       newMessage: "",
-      isValidMessage: true
+      isValidMessage: true,
+      name: ""
     };
   },
   firestore: {
     messages: db.collection("messages").orderBy("createdAt")
+  },
+  created() {
+    this.name = name.findName();
   },
   methods: {
     sendMessage(message) {
@@ -41,7 +47,8 @@ export default {
       }
       db.collection("messages").add({
         createdAt: new Date(),
-        value: message
+        value: message,
+        name: this.name
       });
       this.newMessage = "";
     }
