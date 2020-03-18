41de633d09c6f5eeee92288f731609f81f8d44c0
Ryan Ho <ryan.ho@ucla.edu>
2020-03-18 19:01:59 -0400

    increased vertical margin between messageList messages, decreased margin between message names and message values

--- src/components/MessagesList.vue
+++ src/components/MessagesList.vue
@@ -1,11 +1,11 @@
 <template>
   <ul>
-    <li v-for="(message, index) in messageList" :key="index" class="flex justify-between my-1" v-show="message.value">
+    <li v-for="(message, index) in messageList" :key="index" class="flex justify-between my-4" v-show="message.value">
       <div>
         <span v-if="message.name" class="font-semibold px-2 text-sm text-white rounded-full"
         :class="{'bg-blue-600': activeUser !== message.name, 'bg-orange-500': activeUser === message.name}"
         >{{message.name}}</span>
-        <span class="pl-2">{{message.value}}</span>
+        <span class="pl-1">{{message.value}}</span>
       </div>
       <span class="text-gray-500">{{getRelativeTime(message.createdAt)}}</span>
     </li>
